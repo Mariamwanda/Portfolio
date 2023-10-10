e416e340cf87aab56147c905c56e4fa089d8c0b7
Mariamwanda <mariamwandaogo7@gmail.com>
2023-10-10 14:33:43 +0000

    gogo

---
+++ src/assets/Home.jsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Alert } from "reactstrap";
+function Home() {
+  return (
+    <>
+      <div>
+        <Alert color="info">Salut</Alert>
+      </div>
+    </>
+  );
+}
+
+export default Home;
